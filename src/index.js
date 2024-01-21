a2e1ec0181da7689c06a5af8ee87144c754d553b
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-01-21 17:21:27 -0600

    2 projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import StartRating from "./Star-Rating"
+import Expander from "./Expander"
 // import "./index.css"
 // import App from "./App"
 
@@ -8,9 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating
+    {/* <StartRating
       maxRating={10}
       messages={["Terrible", "Not Bad", "Okay", "Good", "Awesome"]}
-    />
+    /> */}
+    <Expander />
   </React.StrictMode>
 )
